8b009602a43d2e1010848d565738c9f215d021a8
Dries Cools <dries.cools@hotmail.com>
2024-10-31 22:25:01 +0100

    style navigation + remove angular material

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,20 +1,45 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import {MatMenuModule} from '@angular/material/menu';
+import { Component, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, MatMenuModule],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  overlayOpen = false;
+
+  constructor(private renderer: Renderer2) {}
 
   scrollToSection(sectionId: string) {
     const element = document.getElementById(sectionId);
     if (element) {
       element.scrollIntoView({ behavior: 'smooth', block: 'start' });
     }
+  }
+
+  toggleOverlay() {
+    console.log("print")
+    this.overlayOpen = !this.overlayOpen;
+    if (this.overlayOpen) {
+      this.renderer.addClass(document.body, 'no-scroll');
+    } else {
+      this.renderer.removeClass(document.body, 'no-scroll');
+    }
+  }
+
+  closeOverlay() {
+    this.overlayOpen = false;
+    this.renderer.removeClass(document.body, 'no-scroll');
+  }
+
+  navigateTo(sectionId: string) {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+    this.closeOverlay();
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,26 @@ import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+  
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBs1HBizDGr0sD6nhXV1jXce038OhYAQwc",
+  authDomain: "knuffelbus.firebaseapp.com",
+  projectId: "knuffelbus",
+  storageBucket: "knuffelbus.appspot.com",
+  messagingSenderId: "592223707892",
+  appId: "1:592223707892:web:c403bbc842a69ef849b4a1",
+  measurementId: "G-J7Z2N23XLV"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
